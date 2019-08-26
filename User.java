cb11e381097cbcd2e2c8ca292a0838fad747bf24
KiranKamaraj <54532001+KiranKamaraj@users.noreply.github.com>
2019-08-26 17:35:26 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,66 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Table(nmae="mu")
+@Entity
+public class User {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+     
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+     
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + "]";
+	}
+
+	public User(@NotBlank(message = "Name is mandatory") String name,
+			@NotBlank(message = "Email is mandatory") String email) {
+		super();
+		this.name = name;
+		this.email = email;
+	}
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+    
+ 
+
+}
